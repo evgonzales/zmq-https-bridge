df174e7e11f96fa5f9cb220468c4bbdf80071e0b
evgonzales <evgonzales@csumb.edu>
2020-04-14 02:11:56 -0700

    [zmq] ... and fix test scripts

--- test_req_bridge.sh
+++ test_req_bridge.sh
@@ -1,2 +1,2 @@
 screen -S bridge -d -m python3 zmq-http-bridge.py 8181 8080 http://192.168.1.129:8080
-screen -S zmq_app -d -m python3 debug-zmq-req.py 127.0.0.1:8181
+screen -S zmq_app -d -m python3 debug-zmq-req.py tcp://127.0.0.1:8181
