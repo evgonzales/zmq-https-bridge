80b47caf6227242dcc5b1f79a1d7d6e80a168da9
evgonzales <evgonzales@csumb.edu>
2020-04-24 12:56:22 -0700

    Restructure codebase + integrate Twisted partially + functioning REP/REQ tunnel

--- debug-zmq-req.py
+++ debug-zmq-req.py
@@ -1,38 +1,47 @@
-import zmq
 import hashlib
 import random
 import sys
-import time
+from datetime import datetime as time
+
+import zmq
 
 MSG_LENGTH = 16
 
+
 def main():
     if len(sys.argv) != 2:
-        print("usage: %s <protocol://destination:port>" % sys.argv[0])
+        print("usage: %s <dest protocol://dest:port>" % sys.argv[0])
         return
-    
-    sha = hashlib.sha256()
-    
+
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(sys.argv[1])
-    
-    print("[.] Waiting 5 seconds for bridge to start...")
-    time.sleep(5)
-    
-    while True:
-        msg = ''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(MSG_LENGTH))
+
+    start = time.now()
+
+    for req in range(1000):
+        # generate random character sequence for testing
+        msg = ''.join(
+            random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(MSG_LENGTH)
+        )
+
         hash = hashlib.sha256(msg.encode()).hexdigest()
         socket.send(msg.encode())
-        print("[!] Sent new message: \"%s\"" % msg)
-        
+        print("[!] Sent new message: \"%s\" (request #%d)" % (msg, req))
+
         server_hash = socket.recv().decode()
         print("[.] Received digest, validating...")
-        
+
         if hash == server_hash:
             print("[.] Hash test passed.")
         else:
             print("[!] Hash test FAILED. (got %s, expected %s)" % (server_hash, hash))
-            
+
+    end = time.now()
+    delta = end - start
+    print("[*] 1000 requests processed in %d seconds (%f requests per second)"
+          % (delta.seconds, 1000 / delta.seconds))
+
+
 if __name__ == "__main__":
     main()
--- test_req_bridge.sh
+++ test_req_bridge.sh
@@ -1,2 +1,6 @@
-screen -S bridge -d -m python3 zmq-http-bridge.py 8181 8080 http://192.168.1.129:8080
-screen -S zmq_app -d -m python3 debug-zmq-req.py tcp://127.0.0.1:8181
+killall python3
+python3 BridgeApplication.py 127.0.0.1:8181 10.0.1.38:8080 http://10.0.1.84:8080 \
+	> req_out.txt 2>&1 &
+sleep 3
+python3 debug-zmq-req.py tcp://127.0.0.1:8181
+killall python3
